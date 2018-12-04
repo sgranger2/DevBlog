3971db0438defe7d634da259dbd475436fd2bb92
sgranger2 <shaneg457@gmail.com>
2018-12-04 17:11:41 -0600

    Updates

---
+++ precache-manifest.9657fd24d652c3c55b61b8dadd614336.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "94ae879d93b1e3b1d84a",
+    "url": "/DevBlog/static/js/runtime~main.94ae879d.js"
+  },
+  {
+    "revision": "aa104b1e2f9c9934449d",
+    "url": "/DevBlog/static/js/main.aa104b1e.chunk.js"
+  },
+  {
+    "revision": "f7483ca998ce7c4dd588",
+    "url": "/DevBlog/static/js/1.f7483ca9.chunk.js"
+  },
+  {
+    "revision": "aa104b1e2f9c9934449d",
+    "url": "/DevBlog/static/css/main.900b1085.chunk.css"
+  },
+  {
+    "revision": "b4537e49669a821f9523248af9102bb3",
+    "url": "/DevBlog/index.html"
+  }
+];
