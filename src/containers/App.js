f14ecb5a6e21e63626a29e1ce02c7b7f329c97e2
sgranger2 <shaneg457@gmail.com>
2018-12-07 12:10:01 -0600

    Responsive Design Added

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import '../queries.css';
 import Header from '../components/Header';
 import Posts from '../components/Posts';
 import FullPost from '../components/FullPost';
